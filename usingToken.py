23da1d927a8db9eb9f3ebe5dd9bb8b054923812e
venkateshsundi <47320394+venkateshsundi@users.noreply.github.com>
2020-04-06 10:04:55 -0700

    Add files via upload

--- usingToken.py
+++ usingToken.py
@@ -1,6 +1,11 @@
+# import libraries
 from selenium import webdriver
 import requests
 from selenium.webdriver.common.by import By
+import xlrd 
+import xlsxwriter
+
+#initialization
 path = 'chromedriver'
 driver = webdriver.Chrome(path)
 marks =[]
@@ -9,10 +14,23 @@ driver.get(url)
 htnumber = driver.find_element_by_id('ht').send_keys('17jn1a0401')
 search = driver.find_element_by_class_name('ci').click()
 
-htno = "17jn1a0"
+
+
+# input hall ticket worksheet  
+loc = ("E:\python\htNumbers.xlsx") 
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+ 
+# fetch result:scraper  
 token = input('enter: ')
-for i in range(401,460):
-    url = "https://jntuaresults.ac.in/results/res.php?ht="+htno+str(i)+"&id=56736237&accessToken="+token
+for i in range(sheet.nrows):
+    url = "https://jntuaresults.ac.in/results/res.php?ht="+str(sheet.cell_value(i, 0))+"&id=56736237&accessToken="+token
     driver.get(url)
     mark = driver.find_element(by=By.XPATH, value= '/html/body').text
     marks.append([int(s) for s in mark.split() if s.isdigit()])
+
+# write result into worksheet
+with xlsxwriter.Workbook('result.xlsx') as workbook:
+	worksheet = workbook.add_worksheet()
+	for row_num, data in enumerate(marks):
+		worksheet.write_row(row_num, 0, data)
